398a0e56617e3cdbda8b533697b5d87b32244f06
HengkiMac <hengkisaoremaha@gmail.com>
2025-07-20 22:06:10 +0700

    update logo dan tampilan

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <div class="card card-md">
     <div class="card-body">
         <h2 class="h2 text-center mb-4">
-            Login to your account
+            Login ke akun kamu
         </h2>
         <form action="{{ route('login') }}" method="POST" autocomplete="off">
             @csrf
@@ -16,13 +16,13 @@
             <div class="mb-2">
                 <label for="remember" class="form-check">
                     <input type="checkbox" id="remember" name="remember" class="form-check-input"/>
-                    <span class="form-check-label">Remember me on this device</span>
+                    <span class="form-check-label">Remember me</span>
                 </label>
             </div>
 
             <div class="form-footer">
                 <x-button type="submit" class="w-100">
-                    {{ __('Sign in') }}
+                    {{ __('Login') }}
                 </x-button>
             </div>
         </form>
@@ -30,17 +30,17 @@
 </div>
 
 <div class="text-center mt-3 text-gray-600">
-    <p>Don't have an account yet?
+    <!-- <p>Apakah kamu sudah punya akun?
         <a href="{{ route('register') }}" class="text-blue-500 hover:text-blue-700 focus:outline-none focus:underline" tabindex="-1">
-            Sign up
+            Daftar
         </a>
-    </p>
+    </p> -->
 
-    <p class="mt-2">
+    <!-- <p class="mt-2">
         <a href="{{ route('password.request') }}" class="text-sm text-gray-500 hover:text-gray-700 focus:outline-none focus:underline">
-            I forgot my password
+            Saya lupa password
         </a>
-    </p>
+    </p> -->
 </div>
 
 @endsection
